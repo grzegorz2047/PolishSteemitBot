ba24f76e476ec7610edc9378ea89e3d69ce88387
grzegorz2047 <grzegorz2047@gmail.com>
2018-01-29 19:31:02 +0100

    Zabezpieczono przed podwojnym postem temu samemu userowi

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var fs = require('fs');
 //https://steemit.com/piston/@woung717/piston-api-tutorial-make-a-vote-and-post-using-python
 //
 
-
+var currentCheckedId = 0;
 function main() {
 	let path = "credentials.json";
 
@@ -34,36 +34,53 @@ function createFile(path) {
 function runBot(path) {
 		var contents = fs.readFileSync(path);
 		var jsonContent = JSON.parse(contents);
-		console.log("Username:", jsonContent.login);
-		console.log("Password:", jsonContent.password);
-		getNewestPost(jsonContent.login, jsonContent.password);
+		//console.log("Username:", jsonContent.login);
+		//console.log("Password:", jsonContent.password);
+		var myInt = setInterval(function () {
+			getNewestPost(jsonContent.login, jsonContent.password);
+			console.log(" ============= ");
+		}, 1000);
 }
 	
 function getNewestPost(login, password) {
 	steem.api.getDiscussionsByCreated({"tag":"polish","limit":"10"}, function(err, result) {
 		var authorName = result[0]['author'];
+		var postId = result[0]['id'];
 		var authorNames = [authorName];
 		//console.log("Result is " + JSON.stringify(result[0]));
 		//console.log("============================");
 		console.log("Author name is " + authorName);
+		console.log("Post id is " + postId);
 		//console.log(result[0]);
 		steem.api.getAccounts(authorNames, function(err, posts) {
-		  console.log(posts[0]);
+		 // console.log(posts[0]);
 		  var postCount = posts[0]['post_count'];
 		  var commentCount = posts[0]['comment_count'];
 		  console.log("Liczba postow to " + postCount);
-		  console.log("Liczba komentarzy to " + commentCount);
+		  //console.log("Liczba komentarzy to " + commentCount);
+		  if(isAlreadyChecked(postId,currentCheckedId)) {
+			  console.log("Tego juz sprawdzilem");
+			  return;
+		  }
+		
 		  if(commentCount == 1) {
+			currentCheckedId = postId;
 			  var commentForTitle = "";//It's not article
-			  var bodyOfComment = "To jest testowy komentarz";
+			  var bodyOfComment = "Witaj nowy użytkowniku w tagu #polish!";
 			  var wif = steem.auth.toWif(login, password, 'posting');
 			  steem.broadcast.comment(wif, authorName, parentPermlink, botName, permlink, title, bodyOfComment, jsonMetadata, function(err, result) {
 				console.log(err, result);
 			  });
+		  }else {
+			currentCheckedId = postId;
 		  }
 		});
 		
 	 });	
+}
+function isAlreadyChecked(first, second) {
+	console.log("Porownanie " + first + " z " + second); 
+	return first == second;
 }
  
 main();
